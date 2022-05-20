ad2de3919cc290f42dc76c1169c25a1da19eeedd
Rocketmannnnnnn <j.bonefaas@student.avans.nl>
2022-05-20 11:34:33 +0200

    Added commands for text type selection

---
+++ RegexNotepad/Models/DataModel.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RegexNotepad.Models
+{
+    public class DataModel : ObservableObject
+    {
+        public enum TextType { words, sentences, text }
+        public string Text { get; set; }
+        public TextType Type { get; set; }
+    }
+}
---
+++ RegexNotepad/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Input;
+using RegexNotepad.Models;
+using System.Windows.Input;
+using static RegexNotepad.Models.DataModel;
+
+namespace RegexNotepad.ViewModels
+{
+    public class MainWindowViewModel : ObservableObject
+    {
+        public DataModel DataModel { get; set; } = null;
+
+        public MainWindowViewModel()
+        {
+            this.DataModel = new DataModel();
+        }
+
+        private void SetTextType(TextType textType)
+        {
+            this.DataModel.Type = textType;
+        }
+
+        public ICommand SelectWords
+        {
+            get { return new RelayCommand(() => { SetTextType(TextType.words); }); }
+        }
+
+        public ICommand SelectSentences
+        {
+            get { return new RelayCommand(() => { SetTextType(TextType.sentences); }); }
+        }
+
+        public ICommand SelectText
+        {
+            get { return new RelayCommand(() => { SetTextType(TextType.text); });}
+        }
+    }
+}
